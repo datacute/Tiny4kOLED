ac57b96e9cbb0dff88b3bb94ed93fc4fd93d25d2
Stephen Denne <stephen@datacute.co.nz>
2017-04-25 22:10:47 +1200

    Change example to suit 128x32 pixels

--- examples/Tiny4kOLED/Tiny4kOLED.ino
+++ examples/Tiny4kOLED/Tiny4kOLED.ino
@@ -2,9 +2,6 @@
 #include <TinyWireM.h>
 // ============================================================================
 
-#include "digistump_128x64c1.h"
-
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -21,18 +18,15 @@ void loop() {
   delay(1000);
   oled.clear(); //all black
   delay(1000);
+  oled.setFont(FONT8X16); // 2 lines of 16 characters exactly fills 128x32
   //usage: oled.setCursor(X IN PIXELS, Y IN ROWS OF 8 PIXELS STARTING WITH 0);
-  oled.setCursor(0, 0); //top left
-  oled.setFont(FONT8X16);
-  oled.print(F("DIGISTUMP")); //wrap strings in F() to save RAM!
-  oled.setFont(FONT6X8);
-  oled.print(F(" OLED!"));
-  oled.setCursor(0, 2); //two rows down because the 8x16 font takes two rows of 8
-  oled.println(F("test")); //println will move the cursor 8 or 16 pixels down (based on the front) and back to X=0
-  oled.print(F("test test test test test")); //lines auto wrap
+  oled.setCursor(32, 0);
+  oled.print(F("Datacute")); //wrap strings in F() to save RAM!
+  oled.setFont(FONT6X8); // 4 lines of 21 characters only fills 126x32
+  oled.setCursor(13, 2); //two rows down because the 8x16 font takes two rows of 8
+  oled.print(F("Acute Information"));
+  oled.setCursor(16, 3); //cursor x does not need to be a multiple of the font width
+  oled.print(F("Revelation Tools"));
   
-  delay(3000);
-  //usage oled.bitmap(START X IN PIXELS, START Y IN ROWS OF 8 PIXELS, END X IN PIXELS, END Y IN ROWS OF 8 PIXELS, IMAGE ARRAY);
-  oled.bitmap(0, 0, 128, 8, digistumplogo);
   delay(3000);
 }
