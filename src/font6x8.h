cc1957ee82b28a0224947064c05109184082c628
Stephen Denne <stephen@datacute.co.nz>
2018-08-01 14:56:07 +1200

    Adding heading comment with project link to fonts

--- src/font6x8.h
+++ src/font6x8.h
@@ -1,4 +1,11 @@
 /*
+ * Tiny4kOLED - Drivers for SSD1306 controlled dot matrix OLED/PLED 128x32 displays
+ *
+ * Based on ssd1306xled, re-written and extended by Stephen Denne
+ * from 2017-04-25 at https://github.com/datacute/Tiny4kOLED
+ *
+ */
+/*
  * SSD1306xLED - Drivers for SSD1306 controlled dot matrix OLED/PLED 128x64 displays
  *
  * @created: 2014-08-12
